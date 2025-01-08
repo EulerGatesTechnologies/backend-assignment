9fe3d571128219ab8b4dc7b4a880316aef91f7ef
siphamandlakheswa <siphamandlakheswa@outlook.com>
2025-01-08 10:09:50 +0200

    remove OT_Assessment_Data project from application builder

--- src/OT.Assessment.AppHost/Program.cs
+++ src/OT.Assessment.AppHost/Program.cs
@@ -6,6 +6,4 @@ builder.AddProject<Projects.OT_Assessment_Tester>("ot-assessment-tester");
 
 builder.AddProject<Projects.OT_Assessment_App>("ot-assessment-app");
 
-builder.AddProject<Projects.OT_Assessment_Data>("ot-assessment-data");
-
 builder.Build().Run();
