23aa79daa55fba98c32c937d2f5b5acb8086f9ed
siphamandlakheswa <siphamandlakheswa@outlook.com>
2025-01-31 16:27:23 +0200

    Checkpoint, fixed all build errors

--- src/OT.Assessment.AppHost/Program.cs
+++ src/OT.Assessment.AppHost/Program.cs
@@ -1,13 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 // Register a RabbitMQ server
-//var messaging = builder.AddRabbitMQ("messaging");
+var rabbitMq = builder.AddRabbitMQ("messaging");
 
 var playerApi = builder.AddProject<Projects.OT_Assessment_App>("ot-assessment-app");
 //    .WithReference(messaging);
 
 builder.AddProject<Projects.OT_Assessment_Consumer>("ot-assessment-consumer");
-    //.WithReference(messaging);
+//.WithReference(rabbitMq);
 
 // Inject assigned URLs for Player API
 //playerApi.WithEnvironment("PlayerOptions__ApiUrlHostAddress", () => playerApi.GetEndpoint("http").Host);
--- tests/OT.Assessment.UnitTests/OT.Assessment.App/Controllers/PlayerServicesTests.cs
+++ tests/OT.Assessment.UnitTests/OT.Assessment.App/Controllers/PlayerServicesTests.cs
@@ -1,14 +1,17 @@
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
 using OT.Assessment.App.Model;
 
 namespace OT.Assessment.UnitTests.OT.Assessment.App.Model;
 
+
 public class PlayerServicesTests : IClassFixture<WebApplicationFactory<Program>>
 {
-    private readonly PlayerServices _services;
+    private PlayerServices _services;
 
     private readonly WebApplicationFactory<Program> _factory;
 
+    
     public PlayerServicesTests(WebApplicationFactory<Program> factory)
     {
         _factory = factory;
@@ -17,8 +20,14 @@ public class PlayerServicesTests : IClassFixture<WebApplicationFactory<Program>>
     [Fact]
     public void CanCreatePlayerServicesInstance()
     {
-        // Arrange
+        // Precondition
+        Assert.NotNull(_factory);
+
+        // Act
         _services = _factory.Services.GetService<PlayerServices>();
+
+        // Assert
+        Assert.NotNull(_services);
 
     }
 
